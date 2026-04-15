8285098405e23f5cd40b06b3246b1636a39aafe8
Nishida <nishida@libun.co.jp>
2026-04-16 01:10:23 +0900

    v1.26: cat-category3 cream+navy outline for clear distinction, 病院一覧 button static (no hover change)

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('GYOSEI_CHILD_VERSION', '1.25.0');
+define('GYOSEI_CHILD_VERSION', '1.26.0');
 
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style(
