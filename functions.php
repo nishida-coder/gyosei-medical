f17ac4277f300f33e08ba2e85467ef924dcc84ef
Nishida <nishida@libun.co.jp>
2026-06-03 00:11:54 +0900

    v1.53: CRITICAL - separate DR doctor block (#post_list2 with .cat-category) from MEDIA block (without) - restore MEDIA section readable layout

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('GYOSEI_CHILD_VERSION', '1.52.0');
+define('GYOSEI_CHILD_VERSION', '1.53.0');
 
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style(
